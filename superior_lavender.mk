51ab12ffb586c15ce28e12017a3875981b42ed70
Snowman2409 <93831265+Snowman2409@users.noreply.github.com>
2022-04-19 13:59:45 +0530

    Update superior_lavender.mk

--- superior_lavender.mk
+++ superior_lavender.mk
@@ -9,7 +9,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit some common PixelExperience stuff
-$(call inherit-product, vendor/superior/config/common_full_phone.mk)
+$(call inherit-product, vendor/superior/config/common.mk)
 TARGET_BOOT_ANIMATION_RES := 1080
 TARGET_GAPPS_ARCH := arm64
 TARGET_SUPPORTS_GOOGLE_RECORDER := false
