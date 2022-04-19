34387110cb20833ae734fc73799f1165d379a7e0
Snowman2409 <93831265+Snowman2409@users.noreply.github.com>
2022-04-19 13:12:08 +0530

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,7 +1,7 @@
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/xtended_lavender.mk
+    $(LOCAL_DIR)/superior_lavender.mk
 
 COMMON_LUNCH_CHOICES := \
-    xtended_lavender-user \
-    xtended_lavender-userdebug \
-    xtended_lavender-eng
+    superior_lavender-user \
+    superior_lavender-userdebug \
+    superior_lavender-eng
